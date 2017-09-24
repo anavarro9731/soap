4162ec06c7b6208ce730310b152d94af2fb11773
Aaron Navarro <aaron@futuretank.co.uk>
2017-09-24 11:32:36 +0100

    try scripts via modules approach again

--- Palmtree.ApiPlatform/src/load-modules.ps1
+++ Palmtree.ApiPlatform/src/load-modules.ps1
@@ -1,11 +1,11 @@
 
 $vstsCredentials = @("anavarro9731","VST`"04`"supremus")
-$moduleRootUri = "https://anavarro9731.visualstudio.com/defaultcollection/powershell/_apis/git/repositories/powershell/items?api-version=1.0&scopepath="
+$vstsRootUri = "https://anavarro9731.visualstudio.com/defaultcollection/powershell/_apis/git/repositories/powershell/items?api-version=1.0&scopepath="
 $modules = @(
- "build.psm1"
- "prepare-new-version.ps1",
- "build-and-test.ps1",
- "pack-and-publish.ps1"
+ "build",
+ "prepare-new-version",
+ "build-and-test",
+ "pack-and-publish"
 )
 
 Push-Location $PSScriptRoot
@@ -15,9 +15,10 @@ Write-Host "Importing Custom Modules"
 # Base64-encodes the Personal Access Token (PAT) appropriately
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $vstsCredentials[0],$vstsCredentials[1])))
 
+New-Item ".\.psModules\" -ItemType Directory -Verbose
 foreach ($module in $modules) {
- Invoke-RestMethod -Uri "$moduleRootUri$module" -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -ContentType "text/plain; charset=UTF-8" -OutFile $module
- #Import-Module ".\$module" -Verbose -Global -Force
+ Invoke-RestMethod -Uri "$vstsRootUri$file" -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -ContentType "text/plain; charset=UTF-8" -OutFile ".\.psModules\$module\$module.psm1"
+ Import-Module $module -Verbose -Global -Force
  #make sure to .gitgnore .psm1 files as we don't remove this because it is needed by 'Using' statements in other scripts which may also commit changes
 }
 
@@ -31,7 +32,7 @@ function global:Run {
 	)
 	
 	if ($PrepareNewVersion) {
-		.\prepare-new-version.ps1 -projects @(
+		Prepare-NewVersion -projects @(
 			"Palmtree.ApiPlatform.ThirdPartyClients.Mailgun",
 			"Palmtree.ApiPlatform.Interfaces",
 			"Palmtree.ApiPlatform.MessagePipeline",
@@ -46,14 +47,14 @@ function global:Run {
 	}
 
 	if ($BuildAndTest) {
-		.\build-and-test.ps1 -testPackages @(
+		Build-And-Test -testPackages @(
 			"Palmtree.ApiPlatform.Tests",
 			"Palmtree.Sso.Api.Domain.Tests"
 		)
 	}
 
     if ($PackAndPublish) {
-        .\pack-and-publish.ps1 -projectsToPublish @(
+        Pack-And-Publish -projectsToPublish @(
 			"Palmtree.ApiPlatform.ThirdPartyClients.Mailgun",
 			"Palmtree.ApiPlatform.Interfaces",
 			"Palmtree.ApiPlatform.MessagePipeline",
