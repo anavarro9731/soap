c438a0fa8349accdbb433d7cf8a30e01e03aa340
Aaron Navarro <aaron@futuretank.co.uk>
2019-05-03 17:49:57 +0100

    add create new service script

--- src/create-new-service.ps1
+++ src/create-new-service.ps1
@@ -1,15 +1,17 @@
 $NewName = Read-Host -Prompt 'Enter The New Service Name Regex[A-Za-zz\.]'
 
 if ($NewName -match '^[A-Za-z\.]+$') {
+	
+	Write-Host "Please wait..."
 
-	cp ./Soap.Api.Sample ../../$NewName -Recurse -Force 
+	cp ./Soap.Api.Sample "../../$NewName" -Recurse -Force 
 
-	cd ../../$NewName
+	cd "../../$NewName"
 
 	Get-ChildItem -Filter “*Soap.Api.Sample*” -Recurse | Where {$_.FullName -notlike "*\obj\*"} | Where {$_.FullName -notlike "*\bin\*"} |  Rename-Item -NewName {$_.name -replace ‘Soap.Api.Sample’,’Kurve.Api.First’ }  
 
 	Get-ChildItem -Recurse -File -Include *.cs,*.csproj | ForEach-Object {
-		(Get-Content $_).replace('Soap.Api.Sample',$NewName) | Set-Content $_
+		(Get-Content $_).replace('Soap.Api.Sample',"$NewName") | Set-Content $_
 	}
 }
 else {
