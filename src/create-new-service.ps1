4643cfb0b46e1cfea35e77081a918c576e9f39b9
Aaron Navarro <aaron@futuretank.co.uk>
2019-12-08 13:01:28 +0000

    build scripts

--- src/create-new-service.ps1
+++ src/create-new-service.ps1
@@ -1,5 +1,7 @@
 $NewName = Read-Host -Prompt 'Enter The New Service Name (Allowed Characters A-Z,a-z and ".")'
 $DiskLocation = Read-Host -Prompt 'Enter The Target Directory (e.g. c:\code)'
+$PackageFeedUrl = Read-Host -Prompt 'Enter The Nuget Package Feed Url (e.g. https://f.feedz.io/companyA/feed1/nuget)'
+$SymbolsFeedUrl = Read-Host -Prompt 'Enter The Symbols Feed Url (e.g. https://f.feedz.io/companyA/feed1/symbols)'
 
 if (-Not ($NewName -match '^[A-Za-z\.]+$')) {
 	Write-Host "$NewName does not match regex"
@@ -11,6 +13,15 @@ if (-Not ($DiskLocation -match '^(?:[a-zA-Z]\:|\\\\[\w\.]+\\[\w.$]+)\\(?:[\w]+\\
 	Return
 }
 
+if (-Not ($PackageFeedUrl -match 'http|https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)')) {
+	Write-Host "$PackageFeedUrl  is not a valid url path format"
+	Return
+}
+
+if (-Not ($SymbolsFeedUrl -match 'http|https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)')) {
+	Write-Host "$SymbolsFeedUrl is not a valid url path format"
+	Return
+}
 if (-Not (Test-Path -PathType Container $DiskLocation)) {
 	#create it
 	New-Item -ItemType Directory -Force -Path $DiskLocation
@@ -18,18 +29,23 @@ if (-Not (Test-Path -PathType Container $DiskLocation)) {
 	
 Write-Host "Please wait..."
 
-$newLocation = "$DiskLocation".trim('\') + "\$NewName"
+$newLocation = "$DiskLocation".trim('\') + "\$NewName\src"
 
-cp ./Soap.Api.Sample $newLocation -Recurse -Force 
+mkdir $newLocation
+
+cp .\Soap.Api.Sample\**.* $newLocation -Recurse -Force 
 
 cd $newLocation
 
 Get-ChildItem -Filter "*Soap.Api.Sample*" -Recurse | Where {$_.FullName -notlike "*\obj\*"} | Where {$_.FullName -notlike "*\bin\*"} |  Rename-Item -NewName {$_.name -replace "Soap.Api.Sample","$NewName" }  
 
-Get-ChildItem -Recurse -File -Include *.cs,*.csproj | ForEach-Object {
+Get-ChildItem -Recurse -File -Include *.cs,*.csproj,*.ps1 | ForEach-Object {
 	(Get-Content $_).replace('Soap.Api.Sample',"$NewName") | Set-Content $_
 }
 
-dotnet new sln
+(Get-Content .\posh-bootstrap.ps1).replace('##packagefeedurl##', $PackageFeedUrl) | Set-Content .\posh-bootstrap.ps1
+(Get-Content .\posh-bootstrap.ps1).replace('##symbolsfeedurl##', $SymbolsFeedUrl) | Set-Content .\posh-bootstrap.ps1
+
+dotnet new sln -n $NewName
 
 Get-ChildItem -Recurse -File -Filter “*.csproj” | ForEach-Object { dotnet sln add $_.FullName }
--- src/posh-bootstrap.ps1
+++ src/posh-bootstrap.ps1
@@ -1,49 +1,8 @@
-Param(
-    [Parameter(Mandatory=$true)]
-    [Alias('u')]
-	[string] $adoUser,
-    [Parameter(Mandatory=$true)]
-    [Alias('p')]
-    [string] $adoPassword
-)
+Invoke-RestMethod -Uri "https://anavarro9731.visualstudio.com/defaultcollection/powershell/_apis/git/repositories/powershell/items?api-version=1.0&scopepath=load-modules.psm1" -ContentType "text/plain; charset=UTF-8" -OutFile ".\load-modules.psm1"
+Import-Module ".\load-modules.psm1" -Verbose -Global -Force
+Remove-Item ".\load-modules.psm1" -Verbose -Recurse
+Load-Modules
 
-function Go {
-	Load-Modules
-}
-
-function Load-Modules {
-
-	$adoCredentials = @($adoUser,$adoPassword)
-	$adoRootUri = "https://anavarro9731.visualstudio.com/defaultcollection/powershell/_apis/git/repositories/powershell/items?api-version=1.0&scopepath="
-	$modules = @(
-		"build",
-		"prepare-new-version",
-		"build-and-test",
-		"pack-and-publish"
-	)
-
-	# Set Project Root Folder
-	$global:projectRoot = $PSScriptRoot
-	Push-Location $PSScriptRoot
-
-	Write-Host "Importing Custom Modules"
-
-	# Base64-encodes the Personal Access Token (PAT) appropriately
-	$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $adoCredentials[0],$adoCredentials[1])))
-
-	# Set Modules Path for Session
-	$env:PSModulePath = $env:PSModulePath + ";$PSScriptRoot\.psModules\"
-
-	# Download and Import all Modules
-	foreach ($module in $modules) {
-		New-Item ".\.psModules\$module\" -ItemType Directory -Verbose
-		Invoke-RestMethod -Uri "$adoRootUri$module.psm1" -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -ContentType "text/plain; charset=UTF-8" -OutFile ".\.psModules\$module\$module.psm1"
-		Import-Module $module -Verbose -Global -Force
-	}
-
-	#Remove folder once modules are loaded
-	Remove-Item ".\.psModules\" -Verbose -Recurse
-}
 
 #expose this function like a CmdLet
 function global:Run {
@@ -112,5 +71,3 @@ function global:Run {
         -nugetApiKey $nugetApiKey 
     }
 }
-
-Go
