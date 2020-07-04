019013d9fd6cebc0cb90d45f2e6b2b2fdecfdb22
Aaron Navarro <aaron@futuretank.co.uk>
2020-07-04 20:21:04 +1000

    remove

--- src/Soap.Api.Sample/Soap.Api.Sample.Domain.Messages/Events/GotMessageFailedAllRetriesLogItemEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sample.Messages.Events
-{
-    using Soap.Pf.MessageContractsBase.Queries;
-
-    public class GotMessageFailedAllRetriesLogItemEvent : AbstractGetMessageFailedAllRetriesLogItem<
-        GotMessageFailedAllRetriesLogItemEvent>.AbstractResponseEvent
-    {
-    }
-}
