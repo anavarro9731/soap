d9abae13845ad41940c9be38a5127537d3b7a084
Aaron Navarro <aaron@futuretank.co.uk>
2021-01-07 19:13:04 +0000

    fix destrcutor bug

--- src/soapjs/components/modules/src/hooks/useQuery.js
+++ src/soapjs/components/modules/src/hooks/useQuery.js
@@ -3,53 +3,65 @@ import bus from '../soap/bus';
 import commandHandler from '../soap/command-handler';
 import {toTypeName} from "../soap/messages";
 import config from "../soap/config";
+import {uuidv4} from "../soap/util";
+import _ from 'lodash'
 
 export function useQuery({query, sendQuery = true, acceptableStalenessFactorInSeconds = 0}) {
+
+    const [queryResult, setQueryResult] = useState();
+    const [typesLoaded, setTypesLoaded] = useState(false);
+    const [queuedMessage, setQueuedMessage] = useState(false);
     
-        const [queryResult, setQueryResult] = useState();
-        const [typesLoaded, setTypesLoaded] = useState(false);
-        const [queuedMessage, setQueuedMessage] = useState(false);
-                
-        const onResponse = data => {
-            setQueryResult(data);
-        };
+    const onResponse = data => {
+        setQueryResult(data);
+    };
+
+    useEffect(() => {
+        /* right now this is only relevant for autoform C109 command which is not entered by a developer
+        the developer must use the long name for now, or we have to change useEvent, and useCommand as well 
+        and that will also slow things down */
+        const typeName = toTypeName(query.$type);
         
-        useEffect(() => {
-            /* right now this is only relevant for autoform C109 command which is not entered by a developer
-            the developer must use the long name for now, or we have to change useEvent, and useCommand as well 
-            and that will also slow things down */
-            const typeName = toTypeName(query.$type);
-            if (typeName === "types-not-loaded") {
-                if (!queuedMessage) {
-                    console.warn("queueing form til types loaded");
-                    config.startupCallbacks.push(() => {
-                        console.warn("enabling form");
+        let queuedMessageId;
+        
+        if (typeName === "types-not-loaded") {
+            if (!queuedMessage) {
+                console.warn("queueing form until types loaded");
+                queuedMessageId = uuidv4();
+                config.startupCallbacks.push({
+                    f: () => {
+                        console.warn("dequeuing form");
                         setTypesLoaded(true);
-                    });
-                    setQueuedMessage(true);
-                }
-            } else {
-                setTypesLoaded(true);
+                    }, id: queuedMessageId
+                });
+                setQueuedMessage(true);
             }
-            
-            let conversationId = undefined;
-            
-            if (sendQuery && typesLoaded) {  
-                query.$type = typeName; //convert from class short name to assembly qualified short name
-                conversationId = commandHandler.handle(
-                    query,
-                    onResponse,
-                    acceptableStalenessFactorInSeconds,
-                );
+        } else {
+            setTypesLoaded(true);
+        }
+
+        let conversationId = undefined;
+
+        if (sendQuery && typesLoaded) {
+            query.$type = typeName; //convert from class short name to assembly qualified short name
+            conversationId = commandHandler.handle(
+                query,
+                onResponse,
+                acceptableStalenessFactorInSeconds,
+            );
+        }
+
+        //* returning a function is how you set a destructor on the hook's resources 
+        return () => {
+            if (conversationId) {
+                bus.closeConversation(conversationId);
+            }
+            if (queuedMessageId) {
+                _.remove(config.startupCallbacks, i => i.id === queuedMessageId);
             }
 
-            //* returning a function is how you set a destructor on the hook's resources 
-            return () => {
-                if (conversationId) {
-                    bus.closeConversation(conversationId);
-                }
-            };
-        }, [sendQuery, typesLoaded]);
+        };
+    }, [sendQuery, typesLoaded]);
 
-        return queryResult;
+    return queryResult;
 }
