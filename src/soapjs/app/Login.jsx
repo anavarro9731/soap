4102d957981a549129e22f83fd0556ade1db5266
Aaron Navarro <aaron@futuretank.co.uk>
2021-01-18 19:07:41 +0000

    working on auth0, nuget pkg update

--- src/soapjs/app/Login.jsx
+++ src/soapjs/app/Login.jsx
@@ -1,12 +1,23 @@
 import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
-import { Button } from "baseui/button";
+import {useAuth0} from "@auth0/auth0-react";
+import {Button} from "baseui/button";
 
 const Login = () => {
-    
-    const { loginWithRedirect } = useAuth0();
 
-    return <Button onClick={() => loginWithRedirect()}>Log In</Button>;
+    const {logout, loginWithRedirect, user, isAuthenticated, isLoading} = useAuth0();
+
+    if (isLoading) {
+        return;
+    }
+
+    if (isAuthenticated) {
+        return (<React.Fragment>
+            <span>{user.name}</span>
+            <Button onClick={() => logout({returnTo: window.location.origin})}>Logout</Button>
+        </React.Fragment>);
+    } else {
+        return <Button onClick={() => loginWithRedirect()}>Log In</Button>;
+    }
 };
 
 export default Login;
