5ba73a388fe6f5d91e775441c6a27bec7793e30f
Aaron Navarro <aaron@futuretank.co.uk>
2022-08-01 23:20:10 +0100

    update script index

--- src/soapjs/app/index.service-template.js
+++ src/soapjs/app/index.service-template.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import {addTranslations, App, Login, ReactErrorBoundary, config} from '@soap/modules';
+import {App, Login, ReactErrorBoundary, config} from '@soap/modules';
 import {LightTheme} from 'baseui';
 import {H2} from "baseui/typography";   
 import ReactDOM from "react-dom";
-import translations from "./translations/en-soap.app.sample-default";
 import {ALIGN, HeaderNavigation, StyledNavigationItem, StyledNavigationList} from "baseui/header-navigation";
 import {StyledLink} from "baseui/link";
 import {Route, Switch} from "react-router-dom";
-
-addTranslations(translations);
-config.sendMode = "signalr";
 
 function Index() {
     return (
